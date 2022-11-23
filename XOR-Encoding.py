86016b5eb6acdc221fe663fd3b40a141af5da461
fredericlorenz <80405804+fredericlorenz@users.noreply.github.com>
2022-11-23 11:39:23 +0100

    Update XOR-Encoding.py

--- XOR-Encoding.py
+++ XOR-Encoding.py
@@ -15,17 +15,3 @@ print("Msg is " + message + '\n')
 
 def str_xor(s1, s2):
  return "".join([chr(ord(c1) ^ ord(c2)) for (c1,c2) in zip(s1,s2)])
-
-
-
-#Here we do a quick test
-
-enc = str_xor(message, key)
-print('Encrypted messge is: ' + '\n' + enc + '\n')
-
-
-dec = str_xor(enc, key)
-print('Decrypted messge is: ' + '\n' + dec)
-
-
-assert(dec == "testmessage")
