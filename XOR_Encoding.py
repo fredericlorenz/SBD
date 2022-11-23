ecc244c0b8241dd76fa4d4f7f0ac5248f3472668
fredericlorenz <80405804+fredericlorenz@users.noreply.github.com>
2022-11-23 11:54:48 +0100

    Update XOR_Encoding.py

--- XOR_Encoding.py
+++ XOR_Encoding.py
@@ -1,29 +1,13 @@
 import string # The random and string libraries are used to generate a random string with flexible criteria
 import random
-# XOR Encryption
-
-# Random Key Generator
-key = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits + '^!\$%&/()=?{[]}+~#-_.:,;<>|\\') for _ in range(1024))
-
-print(key)
-print('\n' + 'Key length = ' + str (  len(key) ))
 
 
-message = 'testmessage'  # this is the message which we will encrypt before it's getting sent
-print("Msg is " + message + '\n')
-
-
-def str_xor(s1, s2):
- return "".join([chr(ord(c1) ^ ord(c2)) for (c1,c2) in zip(s1,s2)])
-
-#Here we do a quick test
-
-enc = str_xor(message, key)
-print('Encrypted messge is: ' + '\n' + enc + '\n')
-
-
-dec = str_xor(enc, key)
-print('Decrypted messge is: ' + '\n' + dec)
-
-
-assert(dec == "testmessage")
+class Encrypter:
+	@staticmethod
+	def str_xor(s1, s2):
+		return "".join([chr(ord(c1) ^ ord(c2)) for (c1,c2) in zip(s1,s2)])
+	
+	@staticmethod
+	def gen_key():
+		key = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits + '^!\$%&/()=?{[]}+~#-_.:,;<>|\\') for _ in range(1024))
+		return key
